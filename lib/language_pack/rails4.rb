31207bc82cceb3936e64305a42fec0e718df3711
Alexander Mescheryakov <sibsfinx@ya.ru>
2015-04-14 11:22:02 +0300

    fix assets manifest detection

--- lib/language_pack/rails4.rb
+++ lib/language_pack/rails4.rb
@@ -69,7 +69,7 @@ WARNING
   def run_assets_precompile_rake_task
     instrument "rails4.run_assets_precompile_rake_task" do
       log("assets_precompile") do
-        if Dir.glob('public/assets/manifest-*.json').any?
+        if Dir.glob('public/assets/{.,}*-manifest-*.json').any?
           puts "Detected manifest file, assuming assets were compiled locally"
           return true
         end
